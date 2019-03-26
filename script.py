f3c798766f4ea927bb4254b7a9bdb910904b06dd
mikejaus <mikejurie27@gmail.com>
2019-03-26 16:13:54 +1100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+import signal
+
+sensor = 3
+pump = 7
+continue_reading = True
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(pump, GPIO.OUT)
+
+def end_read(signal,frame):
+        global continue_reading
+        print "Aborted Process."
+        continue_process = False
+        GPIO.cleanup(sensor)
+        GPIO.cleanup(pump)
+
+signal.signal(signal.SIGINT, end_read)
+
+while continue_reading:
+        if GPIO.input(sensor)==0:
+                print "Plant needs water!"
+                GPIO.output(pump, GPIO.LOW)
+                time.sleep(1)
+        else:
+                print "Plant does not need to be watered"
+                GPIO.output(pump, GPIO.HIGH)
+                time.sleep(1)
